00e7ea14bc18732b03c96205992378ef682ee8c0
Derry Santoso <derrysantoso7@gmail.com>
2017-06-11 14:12:16 +0700

    Node Hello World test

---
+++ ChatApp/index.js
@@ -0,0 +1,9 @@
+var app = require('express')();
+var server = require('http').Server(app);
+
+//port 3000
+server.listen(3000);
+
+app.get('/', function(request,response) {
+	response.send('Hello World');
+})
