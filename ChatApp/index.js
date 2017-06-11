1cf3fb735884ee0950940dd2228710c3b7466f06
Derry Santoso <derrysantoso7@gmail.com>
2017-06-11 14:32:39 +0700

    socket io connection test

--- ChatApp/index.js
+++ ChatApp/index.js
@@ -1,9 +1,16 @@
 var app = require('express')();
 var server = require('http').Server(app);
+var io = require('socket.io')(server);
 
 //port 3000
 server.listen(3000);
 
 app.get('/', function(request,response) {
 	response.sendFile(__dirname + '/index.html');
-})
+});
+
+//when a connection between the client and server is made. 
+//when we have a connection, we will trigger this:
+io.on('connection', function(socket) {
+	console.log('A connection was made.');
+});
